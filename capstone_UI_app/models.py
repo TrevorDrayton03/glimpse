67090415ba1110f54ef4cc25800a869ddf7d20a0
Trevor Drayton <trevorpdrayton@gmail.com>
2024-01-04 10:43:33 -0800

    included a comment on pillows

--- capstone_UI_app/models.py
+++ capstone_UI_app/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import User
 
 class UserImage(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # image = models.ImageField(upload_to='user_images/')
+    # image = models.ImageField(upload_to='user_images/') # this requires pilows to be installed
